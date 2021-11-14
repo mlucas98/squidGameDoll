25e8072eac6354ec9ef4997b2796a43db4768638
mlucas98 <lukymassaro@gmail.com>
2021-11-14 01:53:39 -0300

    tired of routes

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ function delay(time) {
 class Doll {
     constructor() {
         loader.load(
-            '/models/squid_game/scene.gltf'
+            'https://mlucas98.github.io/squidGameDoll/models/squid_game/scene.gltf'
            , (gltf) => {
                scene.add( gltf.scene );
                gltf.scene.scale.set(.4, .4, .4);
